ec81e88579252148b293e3396b91f47542ebd31b
Rikuto Iitsuka <s1711426@s.tsukuba.ac.jp>
2019-06-07 11:32:51 +0900

    herokuを使う

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get'book/read',to:'book#read'
+  get'book/read'
+  root 'book#read'
 end
