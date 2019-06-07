37a9f7b73c49f6ea78c6abf08350df7ad6063b7d
Rikuto Iitsuka <s1711426@s.tsukuba.ac.jp>
2019-06-07 13:57:02 +0900

    ログイン機能の反映

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'comics/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'book/read'
   root 'book#read'
   post 'comics/create'
+  get'/login',to: 'sessions#new'
+  post'sessions/create'
 end
