9b98e4ddbd26ce233fe8d978678842a7ccb48a00
Rikuto Iitsuka <s1711426@s.tsukuba.ac.jp>
2019-06-07 13:09:53 +0900

    フォーム入力の反映

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,18 @@
 class ComicsController < ApplicationController
   def new
   end
+
+  def create
+   comic = Comic.new(comic_params)
+   comic.save
+   flash[:success] = "#{comic.title}を追加しました"
+   redirect_to root_path
+ end
+
+ private
+
+ def comic_params
+   params.require(:comic).permit(:title)
+ end
+ 
 end
